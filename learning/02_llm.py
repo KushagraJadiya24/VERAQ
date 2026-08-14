26ae1ce418455b90990f6f89379326096470a807
Kushagra Jadiya <kjadiya2004@gmail.com>
2026-08-15 01:02:33 +0530

    system instructions and temperature experimented

--- learning/02_llm.py
+++ learning/02_llm.py
@@ -4,17 +4,20 @@ from google import genai
 
 load_dotenv()
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-print(os.getenv("GEMINI_API_KEY"))
 def ask_llm(prompt):
     try:
         response = client.models.generate_content(
             model="gemini-3.5-flash-lite",
-            contents=prompt
+            contents=prompt,
+            config={
+                "system_instruction": "You are a Dragon. Answer like a dragon",
+                "temperature": 1}
         )
+     
         return response.text
     except Exception as e:
         return f"Request failed: {e}"
 
 if __name__ == "__main__":
-    answer = ask_llm("What is 2 + 2, and explain why in one line.")
-    print(answer)
+        answer = ask_llm("hi, how are you")
+        print(answer)
